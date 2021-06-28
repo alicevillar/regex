74949d866c3791963ee3f1cd2f268d19f9c3acf7
Alice Villar <alsaldanhavillar@gmail.com>
2021-06-28 13:16:59 -0300

    first commit

---
+++ regex2.py
@@ -0,0 +1,12 @@
+# To check a list of CEPs
+
+import re
+
+value = ['M1 1AA', 'M60 1NW', 'CR2 6XH', 'DN55 1PT', 'W1A 1HQ', 'EC1A 1BB']
+
+for element in value:
+    ceps = re.match(r"^(([\w]{2})|([\w]{3})|([\w]{4}))([\s])(([\w]{3})$)", element)
+    if ceps:
+        print(f"Cep {element} is valid")
+
+
